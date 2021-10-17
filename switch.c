8c24f9fc740dc1f86c2cf3264b99da9e243def58
digitalizedHyperstition <joellarssonpriv@gmail.com>
2021-10-17 03:41:22 +0100

    simple gui in pure winapi

--- switch.c
+++ switch.c
@@ -1,14 +1,23 @@
+// I know that pure winapi gui is cancer, will switch to qt or dx rendering when i get the motivation and time
 #include <stdio.h>
 #include <windows.h>
 #include <WinReg.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+#define ID_TOGGLE 1
+#define BUFFER 8192
 
 const char* KEYPATH = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize";
 const char* APPTHEME = "AppsUseLightTheme";
 const char* SYSTHEME = "SystemUsesLightTheme";
 
-const char* setTheme(DWORD val)
+const char* setTheme()
 {
     HKEY hKey;
+    DWORD val;
+    DWORD holder;
+    DWORD buff = BUFFER;
 
     if(RegCreateKeyEx(HKEY_CURRENT_USER, KEYPATH, 0, NULL, REG_OPTION_NON_VOLATILE, KEY_SET_VALUE, NULL, &hKey, NULL) != ERROR_SUCCESS)
     {
@@ -22,6 +31,12 @@ const char* setTheme(DWORD val)
         return "failed to open reg directory.";
     }
 
+    RegGetValueA(HKEY_CURRENT_USER, KEYPATH, APPTHEME, RRF_RT_DWORD, NULL, (PVOID)&holder, &buff);
+    if(holder == 0)
+        val = 1;  
+    else 
+        val = 0;
+
     if(RegSetValueEx(hKey, APPTHEME, 0, REG_DWORD, (const BYTE*)&val, sizeof(val)) == ERROR_SUCCESS && RegSetValueEx(hKey, SYSTHEME, 0, REG_DWORD, (const BYTE*)&val, sizeof(val)) == ERROR_SUCCESS)
     {
         RegCloseKey (hKey);
@@ -32,18 +47,57 @@ const char* setTheme(DWORD val)
     return "Something went wrong bud.";
 }
 
-int main(int argc, char* argv[])
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, int nCmdShow)
 {
-    while(1)
+    MSG msg;
+    WNDCLASSW wc = {0};
+    wc.lpszClassName = L"Theme Switch";
+    wc.hInstance = hInstance;
+    wc.hbrBackground = GetSysColorBrush(COLOR_3DFACE);
+    wc.lpfnWndProc = WndProc;
+    wc.hCursor = LoadCursor(0, IDC_ARROW);
+
+    RegisterClassW(&wc);
+    CreateWindowW(wc.lpszClassName, L"",
+                  WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+                  150, 150, 150, 100, 0, 0, hInstance, 0);
+
+    while (GetMessage(&msg, NULL, 0, 0))
     {
-        DWORD answer;
-
-        puts("1 for lightmode, 0 for darkmode");
-        puts("Ctrl+C to exit\n");
-
-        printf("> ");
-        scanf("%d", &answer);
-        printf("%s\n", setTheme(answer));
+    
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
     }
-    return 0;
+
+    return (int) msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    
+    switch(msg) {
+
+        case WM_CREATE:
+
+            CreateWindowW(L"Button", L"Switch Theme",
+                WS_VISIBLE | WS_CHILD ,
+                15, 15, 100, 25, hwnd, (HMENU) ID_TOGGLE, NULL, NULL);
+            break;
+
+        case WM_COMMAND:
+
+            if (LOWORD(wParam) == ID_TOGGLE) {
+            
+                setTheme();
+            }
+
+            break;
+
+        case WM_DESTROY:
+
+            PostQuitMessage(0);
+            break;
+    }
+
+    return DefWindowProcW(hwnd, msg, wParam, lParam);
 }
